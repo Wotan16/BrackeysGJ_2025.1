215be202cc795d4531e17825b528c89e81b79f7c
Dmytro Drobynko <dmytrodrobynko@gmail.com>
2025-02-18 18:28:32 +0200

    Added dash and base code for attack and parry

---
+++ HotlineJapan3/Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    public event Action OnAttackAnimationEnded;
+
+    [SerializeField] private Animator animator;
+    private const string ATTACK_TRIGGER = "Attack";
+    private const string PARRY_BOOL = "IsParrying";
+    private const string DASHING_BOOL = "Dash";
+    private const string MOVING_BOOL = "IsMoving";
+    private const string DEATH_TRIGGER = "Death";
+
+    public void Attack()
+    {
+        animator.SetTrigger(ATTACK_TRIGGER);
+    }
+
+    public void SetParrying(bool parrying)
+    {
+        animator.SetBool(PARRY_BOOL, parrying);
+    }
+
+    public void Die()
+    {
+        animator.SetTrigger(DEATH_TRIGGER);
+    }
+
+    public void SetDashing(bool dashing)
+    {
+        animator.SetBool(DASHING_BOOL, dashing);
+    }
+
+    public void SetMoving(bool moving)
+    {
+        animator.SetBool(MOVING_BOOL, moving);
+    }
+
+    #region AnimationEvents
+
+    public void OnAttackAnimationEnded_Anim()
+    {
+        OnAttackAnimationEnded?.Invoke();
+    }
+
+    #endregion
+}
--- HotlineJapan3/Assets/Scripts/Player/PlayerController.cs
+++ HotlineJapan3/Assets/Scripts/Player/PlayerController.cs
@@ -7,26 +7,104 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance {  get; private set; }
 
+    public bool ControlledByPlayer = true;
+
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
     private Rigidbody2D rb2D;
     private Vector2 mousePosition;
     private Vector2 inputDireciton;
-    public bool ControlledByPlayer = true;
+
+    [Header("Dash")]
+    [SerializeField] private float dashDistance;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashCooldown;
+    private float dashDistanceDelta;
+    private float dashCDDelta;
+    private bool canDash => dashCDDelta <= 0;
+    private bool dashing => dashDistanceDelta < dashDistance;
+    private Vector2 dashDirection;
+
+    [Header("Attack")]
+    [SerializeField] private float attackCooldown;
+    private float attackCDDelta;
+    private bool canAttack => attackCDDelta <= 0;
+    private bool attacking;
+
+    [Header("Parry")]
+    [SerializeField] private float parryDuration;
+    private float parryCDDelta;
+    private bool canParry => parryCDDelta <= 0;
+    private bool parrying;
+
+    [SerializeField] private PlayerAnimator animator;
+    
+    private bool isBusy => attacking || dashing;
 
     private void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        animator.OnAttackAnimationEnded += Animator_OnAttackAnimationEnded;
+    }
+
+    private void Animator_OnAttackAnimationEnded()
+    {
+        if (!attacking)
+            return;
+
+        attacking = false;
+    }
+
     private void Update()
     {
+        HandleCooldowns();
+        if (dashing)
+            return;
         HandleMovement();
         HandleRotation();
     }
 
+    private void FixedUpdate()
+    {
+        if (dashing)
+        {
+            HandleDashing();
+        }
+    }
+
+    private void HandleCooldowns()
+    {
+        if(parryCDDelta > 0)
+        {
+            parryCDDelta -= Time.deltaTime;
+        }
+
+        if (dashCDDelta > 0)
+        {
+            dashCDDelta -= Time.deltaTime;
+        }
+
+        if (attackCDDelta > 0)
+        {
+            attackCDDelta -= Time.deltaTime;
+        }
+    }
+
+    private void HandleDashing()
+    {
+        float moveAmount = Time.fixedDeltaTime * dashSpeed;
+        Vector2 moveVector = dashDirection * moveAmount;
+        rb2D.MovePosition(rb2D.position + moveVector);
+        dashDistanceDelta += moveAmount;
+    }
+
     private void HandleMovement()
     {
-        rb2D.linearVelocity = inputDireciton * moveSpeed;
+        rb2D.linearVelocity = inputDireciton.normalized * moveSpeed;
     }
 
     private void HandleRotation()
@@ -34,6 +112,37 @@ public class PlayerController : MonoBehaviour
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector2 direction = (mouseWorldPosition - transform.position).normalized;
         transform.up = direction;
+    }
+
+    private void Attack()
+    {
+        if (isBusy || !canAttack)
+            return;
+
+        parrying = false;
+
+        //animator.Attack();
+    }
+
+    private void Dash()
+    {
+        if (isBusy || !canDash || inputDireciton == Vector2.zero)
+            return;
+
+        dashDistanceDelta = 0;
+        dashCDDelta = dashCooldown;
+        parrying = false;
+        dashDirection = inputDireciton.normalized;
+        //animator.SetDashing(dashing);
+    }
+
+    private void Parry()
+    {
+        if(isBusy)
+            return;
+
+        parrying = true;
+        //animator.SetParrying(parrying);
     }
 
     #region InputEvents
@@ -61,6 +170,8 @@ public class PlayerController : MonoBehaviour
 
         if (!context.started)
             return;
+
+        Attack();
     }
 
     public void OnParry_Input(InputAction.CallbackContext context)
@@ -70,6 +181,8 @@ public class PlayerController : MonoBehaviour
 
         if (!context.started)
             return;
+
+        Parry();
     }
 
     public void OnDash_Input(InputAction.CallbackContext context)
@@ -79,6 +192,8 @@ public class PlayerController : MonoBehaviour
 
         if (!context.started)
             return;
+
+        Dash();
     }
 
     #endregion
